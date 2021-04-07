92d24c58c6385faddfe22daf84981c3a6de9a5e6
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-04-07 11:33:20 -0700

    Progress so far of Project Two.

--- projectTwo/ViewController.swift
+++ projectTwo/ViewController.swift
@@ -5,15 +5,61 @@
 //  Created by Berdell Akootchook on 3/28/21.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:UIViewController {
+
+    @IBOutlet weak var selfPortrait: UIImageView!
+   
+    @IBOutlet weak var uhWest: UIImageView!
+    
+    @IBOutlet weak var myName: UILabel!
+    
+    @IBOutlet weak var nameName: UILabel!
+   
+    @IBOutlet weak var myMajor: UILabel!
+    
+    @IBOutlet weak var majorMajor: UILabel!
+    
+    @IBOutlet weak var mySpring: UILabel!
+    
+    @IBOutlet weak var textBox: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        title = "About Me"
+  
+        selfPortrait.image = UIImage(named:"Self Portrait")
+//     my photo
+        
+        uhWest.image = UIImage(named:"UHWO")
+//        sets UHWO symbol
+        
+        self.myName.text = "My Name is:"
+        
+        self.nameName.text = "Berdell Akootchook"
+        
+        self.myMajor.text = "My Majors are:"
+        
+        self.majorMajor.text = " BS Information Assurance and Security"
+        
+        self.mySpring.text = "My Spring 2021 Classes Are:"
+        
+        self.textBox.text = "My Classes are CM 161 Introduction to Mobile IOS App Development."
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
 }
 
+//View 1
